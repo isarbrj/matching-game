ba877f5d4065bbbb4850572c75388e24b304d67a
Isabella <isabella7ma@gmail.com>
2022-04-07 19:30:51 -0300

    :tada: changed project base

---
+++ src/components/CardGame/PlayerScore/index.js
@@ -0,0 +1,10 @@
+import './style.css';
+function CardGame(){
+    return `
+    <article class="card-game">
+        <img src="src/images/alura-pixel.png" alt="logo da alura">
+    </article>
+    `;
+}
+
+export default CardGame;
